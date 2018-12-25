e59d3419437d60f59383de61964ecc8afc8c9545
Francis Bongiovanni <fbongiovanni29@gmail.com>
2018-12-24 21:13:01 -0500

    Add secrets

--- boot.rb
+++ boot.rb
@@ -1,6 +1,7 @@
 APP_PATH = '/home/pi/device_controller'
 require "#{APP_PATH}/config/application.rb" 
 CONFIG = YAML.load_file("#{APP_PATH}/config/config.yml")
+CONFIG.deep_merge!(YAML.load_file("#{APP_PATH}/config/secrets.yml"))
 binding.pry if ARGV.first == 'debug'
 
 log = false
